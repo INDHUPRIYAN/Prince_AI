b1ec02d9fb206880b222dd9595af743b67e0e551
Indhu Priyan <indhupriyan1726@gmail.com>
2026-02-18 07:54:59 +0530

    commit ai front

---
+++ prince-frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+// src/pages/Home.tsx
+import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+
+function Home() {
+  return (
+    <Box p={4}>
+      <Text fontSize="2xl" fontWeight="bold">
+        Welcome to Prince AI 🧠✨
+      </Text>
+    </Box>
+  )
+}
+
+export default Home
